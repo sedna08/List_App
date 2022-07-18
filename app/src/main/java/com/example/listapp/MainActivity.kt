387b43084e42b0d6d704acf4196fcf9615c4bf52
sedna <ochiamitcham@gmail.com>
2022-07-18 17:20:49 +0800

    Test 5 Commit From Mitcham

--- app/src/main/java/com/example/listapp/MainActivity.kt
+++ app/src/main/java/com/example/listapp/MainActivity.kt
@@ -12,26 +12,8 @@ class MainActivity : AppCompatActivity() {
 
         binding.textView.text = "Hello There"
 
-        val zero = 5
-        val one = 1
-        val two = 2
-        val three = 3
-        val four = 4
-        val five = 5
-        val six =6
-        val seven =7
-        val esit = 6
-        val soafj = 9
-
         binding.textView.text = "Edited This"
 
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
+        binding.textView.text = "And This"
     }
 }
