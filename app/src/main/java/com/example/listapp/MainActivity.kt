c353bb65a90c5af5895b69ea0cb61e5e4df511d9
sedna <ochiamitcham@gmail.com>
2022-07-18 17:39:34 +0800

    Reset

--- app/src/main/java/com/example/listapp/MainActivity.kt
+++ app/src/main/java/com/example/listapp/MainActivity.kt
@@ -10,19 +10,6 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val zero = 5
-        val one = 1
-        val two = 2
-        val three = 3
-        val four = 4
-        val five = 5
-        val six =6
-        val seven =7
-        val esit = 6
-        val soafj = 9
-
-        binding.textView.text = "And This here"
-
 
     }
 }
