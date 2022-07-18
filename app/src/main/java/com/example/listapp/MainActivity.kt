eb793aa2164bf9315e6bef9aeb1c8481a7ccabd6
sedna <ochiamitcham@gmail.com>
2022-07-18 17:21:56 +0800

    Test 6 Commit

--- app/src/main/java/com/example/listapp/MainActivity.kt
+++ app/src/main/java/com/example/listapp/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.textView.text = "Edited This"
 
-        binding.textView.text = "And This"
+        binding.textView.text = "And This here"
+
     }
 }
