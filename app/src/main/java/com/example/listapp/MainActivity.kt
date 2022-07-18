bcce53df43684fdbfb9652f7853c11db26bd66d0
sedna <ochiamitcham@gmail.com>
2022-07-18 17:37:38 +0800

    Test 8 Commit

--- app/src/main/java/com/example/listapp/MainActivity.kt
+++ app/src/main/java/com/example/listapp/MainActivity.kt
@@ -10,8 +10,6 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.textView.text = "Hello There"
-
         val zero = 5
         val one = 1
         val two = 2
@@ -23,16 +21,8 @@ class MainActivity : AppCompatActivity() {
         val esit = 6
         val soafj = 9
 
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
-        binding.textView.text = "Edited This"
-
         binding.textView.text = "And This here"
+
 
     }
 }
